b59034e7058c6bf5a8f5ed06cf2d597b121106b5
Neri-Laredo <nerila1310@gmail.com>
2020-03-19 12:46:06 -0600

    Colocamos los Gastos en el State de Gastos

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ function App() {
     const [presupuesto, guardarPresupuesto] = useState(0);
     const [restante, guardarRestante] = useState(0);
     const [mostrarpregunta, actualizarPregunta ] = useState(true);
+    const [gastos, guardarGastos] = useState([]);
+
+    //cuando agreguemos un nuevo gasto
+    const agregarNuevoGasto = gasto => {
+        guardarGastos([
+            ...gastos,
+            gasto
+        ])
+    }
 
     return (
         <div>
@@ -25,7 +34,9 @@ function App() {
                         : (
                             <div className="row">
                                 <div className="one-half column">
-                                    <Formulario />
+                                    <Formulario 
+                                        agregarNuevoGasto={agregarNuevoGasto}
+                                    />
                                 </div>
                             
                                 <div className="one-half column">
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import Error from './Error';
 import shortid from 'shortid';
 
-const Formulario = () => {
+const Formulario = ({agregarNuevoGasto}) => {
 
     const [nombre, guardarNombre] = useState('');
     const [cantidad, guardarCantidad] = useState(0);
@@ -26,10 +26,12 @@ const Formulario = () => {
             id: shortid.generate()
         }
 
-
         //pasar el gasto al componente principal
+        agregarNuevoGasto(gasto);
 
         //reserear el form
+        guardarNombre('');
+        guardarCantidad('0');
 
     }
 

9c69a72b7408edaa971d35074ae2eb91bd9419cd
Neri-Laredo <nerila1310@gmail.com>
2020-03-19 12:39:06 -0600

    Creamos un nuevo gasto e instalamos shortid

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Error from './Error';
+import shortid from 'shortid';
 
 const Formulario = () => {
 
@@ -19,6 +20,12 @@ const Formulario = () => {
         guardarError(false);
 
         //construir el gasto
+        const gasto = {
+            nombre,
+            cantidad,
+            id: shortid.generate()
+        }
+
 
         //pasar el gasto al componente principal
 

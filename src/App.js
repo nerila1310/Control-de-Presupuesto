9c58988a5a5cfdfd7428ba9a0e4b1d682ddd694a
Neri-Laredo <nerila1310@gmail.com>
2020-03-19 11:54:09 -0600

    Condicionando vista de formularios

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     //definir el state
     const [presupuesto, guardarPresupuesto] = useState(0);
     const [restante, guardarRestante] = useState(0);
+    const [mostrarpregunta, actualizarPregunta ] = useState(true);
 
     return (
         <div>
@@ -14,19 +15,27 @@ function App() {
                 <h1>Gasto Semanal</h1>
                 
                 <div className="contenido-principal contenido">
-                    <Pregunta 
-                        guardarPresupuesto={guardarPresupuesto}
-                        guardarRestante={guardarRestante}
-                    />
+                    {mostrarpregunta 
+                        ? (
+                            <Pregunta 
+                                guardarPresupuesto={guardarPresupuesto}
+                                guardarRestante={guardarRestante}
+                                actualizarPregunta={actualizarPregunta}
+                            />)   
+                        : (
+                            <div className="row">
+                                <div className="one-half column">
+                                    <Formulario />
+                                </div>
+                            
+                                <div className="one-half column">
+                                    2
+                                </div>
+                            </div>
+                        )
+                    }
 
-                    <di className="row">
-                        <div className="one-half column">
-                            <Formulario />
-                        </div>
-                        <div className="one-half column">
-                            2
-                        </div>
-                    </di>
+                    
                 </div>
 
             </header>
